21da1e1e97364d14c13e134fa4c5edd497d4e8e1
jwinding <jacobkwinding@gmail.com>
2019-04-16 16:14:47 +0900

    initial commit

---
+++ RedditScraperGUI.py
@@ -0,0 +1,405 @@
+import sys, os
+import datetime
+from PyQt5.QtCore import Qt, QThread, pyqtSignal, pyqtSlot, QModelIndex
+import redditScraper
+import configparser
+from PyQt5.QtGui import QPixmap, QIntValidator, QIcon
+from PyQt5.QtWidgets import (QWidget, QApplication,
+                             QGridLayout, QLabel,
+                             QPushButton,QAction,
+                             QLineEdit, QMessageBox,
+                             QFileDialog, QTextEdit,
+                             QFileSystemModel, QTreeView,
+                             QHBoxLayout, QMenuBar,
+                             QComboBox, QDialog ,
+                             QSizePolicy)
+
+class LoginInfoWindow(QDialog):
+    """An input form to get the users reddit login information."""
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
+
+        self.initUI()
+
+    def initUI(self):
+        # define components
+        nameInput = QLineEdit()
+        passwordInput = QLineEdit()
+        infoLabel = QLabel('To use the scraper, you need to have a valid reddit account.')
+        nameLabel = QLabel('username')
+        passwordLabel = QLabel('password')
+
+
+        self.nameInput=nameInput
+        self.passwordInput=passwordInput
+
+        submitButton = QPushButton('Submit')
+        exitButton = QPushButton('Quit')
+
+        # setup the grid layout
+        grid = QGridLayout()
+        grid.setSpacing(4)
+        grid.addWidget(infoLabel, 1, 0, 1,2)
+        grid.addWidget(nameLabel, 2, 0)
+        grid.addWidget(nameInput, 2, 1)
+        grid.addWidget(passwordLabel, 3, 0)
+        grid.addWidget(passwordInput, 3, 1)
+        grid.addWidget(submitButton,4,0)
+        grid.addWidget(exitButton, 4,1)
+
+        self.setLayout(grid)
+
+
+
+        submitButton.clicked.connect(self.submitData)
+        exitButton.clicked.connect(self.closeOrExit)
+
+        self.setGeometry(100, 300, 400, 170)
+        self.setWindowTitle('Reddit Image Scraper')
+        self.exec()
+
+    def submitData(self):
+        if self.nameInput.text() and self.passwordInput.text():
+            self.parent.username = self.nameInput.text()
+            self.parent.password = self.passwordInput.text()
+            self.close()
+        else:
+            msgBox = QMessageBox()
+            msgBox.setText('Please provide a valid username and password.')
+            msgBox.setWindowTitle("Invalid information")
+            msgBox.exec_()
+        pass
+
+    def closeOrExit(self):
+        if 'USER' in self.parent.config:
+            self.close()
+        else:
+            sys.exit()
+
+class RedditScraperWindow(QWidget):
+    """The main window of the program."""
+
+########### Setup ##################################
+    def __init__(self):
+        super().__init__()
+
+        self.readUserConfig()
+        self.initUI()
+
+    def initUI(self):
+        """sets up the user interface, connects all the signals and shows the window. """
+
+        self.init_components()
+        self.readSettingsConfig()
+        self.connect_signals()
+
+        self.setGeometry(100, 100, 1000, 800)
+        self.setWindowTitle('Reddit Image Scraper')
+        self.setWindowIcon(QIcon('redditIcon.png'))
+
+        self.show()
+
+    def init_components(self):
+        """initializes all components and sets up the layout"""
+        internalWidget = QWidget()
+
+        ############ define components ####################
+        self.subredditInput = QLineEdit()
+        self.numInput = QLineEdit()
+        self.onlyInt = QIntValidator()
+        self.numInput.setValidator(self.onlyInt)
+        subredditLabel = QLabel('subreddit')
+        numLabel = QLabel('number of images')
+        self.dirLabel = QLabel('base download dir')
+
+        self.imgView = QLabel()
+
+        self.outputText = QTextEdit('')
+        self.outputText.setReadOnly(True)
+
+        self.sortingCb = QComboBox()
+        self.sortingCb.addItems(["Hot", "Top all time",
+                                 "Top this month", "Top past year",
+                                 "New", "Controversial"])
+        sortingLabel = QLabel('sorting method')
+        self.runButton = QPushButton('Download images')
+        self.chooseDirButton = QPushButton('chose download dir')
+        self.stopButton = QPushButton('Stop download')
+
+        self.fileModel = QFileSystemModel()
+        self.tree = QTreeView()
+        self.tree.setModel(self.fileModel)
+
+        ############## Menu stuff ###################
+        menu_bar = QMenuBar()
+        file_menu = menu_bar.addMenu('File')
+        help_menu = menu_bar.addMenu('Help')
+        self.edit_login_action = QAction('Edit reddit login', self)
+        file_menu.addAction(self.edit_login_action)
+
+        self.exit_action = QAction('Exit', self)
+
+        file_menu.addAction(self.exit_action)
+        self.help_action = QAction('Help', self)
+        help_menu.addAction(self.help_action)
+        menu_bar.setFixedHeight(30)
+        ###########################################################
+
+        ############# Setup the grid layout###############################
+        grid = QGridLayout()
+        # grid.addWidget(menu_bar, 1, 0, 1, 4)
+        grid.setSpacing(4)
+        grid.addWidget(subredditLabel, 1,0)
+        grid.addWidget(self.subredditInput, 1,1)
+        grid.addWidget(numLabel, 2,0)
+        grid.addWidget(self.numInput,2,1)
+        grid.addWidget(sortingLabel, 3,0)
+        grid.addWidget(self.sortingCb,3,1)
+
+        grid.addWidget(self.chooseDirButton,4,0)
+        grid.addWidget(self.dirLabel,4,1)
+        grid.addWidget(self.stopButton,5,0)
+        grid.addWidget(self.runButton,5,1)
+        grid.addWidget(self.outputText,6,0,6,2)
+        grid.addWidget(self.tree,1,3, 11,3)
+
+        #the image viewer, setting how it behaves under resizing.
+        self.imgView.setSizePolicy(QSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding))
+        self.imgView.setMaximumHeight(800)
+
+        internalWidget.setLayout(grid)
+        internalWidget.setMinimumWidth(700)
+        internalWidget.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.MinimumExpanding))
+
+        hbox = QHBoxLayout()
+        hbox.setMenuBar(menu_bar)
+        hbox.addWidget(internalWidget)
+        hbox.addWidget(self.imgView)
+        self.setLayout(hbox)
+
+    def connect_signals(self):
+        """connects all the signals to the right functions"""
+        self.chooseDirButton.clicked.connect(self.showDirDialog)
+        self.runButton.clicked.connect(self.runDownloadThreaded)
+        self.tree.clicked.connect(self.on_treeView_clicked)
+        self.stopButton.clicked.connect(self.stopDownload)
+
+        self.exit_action.triggered.connect(exit)
+
+        self.edit_login_action.triggered.connect(self.edit_login_info)
+        self.help_action.triggered.connect(self.helpClicked)
+
+    def readUserConfig(self):
+        """reads in the users username and password from the config file, or if there is no config file,
+        shows an input dialog.
+        Also tests so that only valid login information gets saved to the config file. """
+        config = configparser.ConfigParser()
+
+        if os.path.exists('redditScraper.ini'):
+            config.read('redditScraper.ini')
+
+            self.username = config['USER']['username']
+            self.password = config['USER']['password']
+            self.redditScraper = redditScraper.redditScraper(self.username,
+                                                             self.password)
+            if 'DIR' in config:
+                self.folder = config['DIR']['root_folder']
+
+        else:
+            while True:
+                LoginInfoWindow(self)
+                self.redditScraper = redditScraper.redditScraper(self.username,
+                                                                 self.password)
+                if self.redditScraper.valid_login():
+                    break
+                else:
+                    msgBox = QMessageBox()
+                    msgBox.setText('Please provide a valid username and password.')
+                    msgBox.setWindowTitle("Invalid information")
+                    msgBox.exec_()
+
+
+
+            config['USER'] = {
+                'username' : self.username,
+                'password' : self.password
+            }
+
+            with open('redditScraper.ini', 'w') as configfile:
+                config.write(configfile)
+
+        self.config=config
+
+    def readSettingsConfig(self):
+        """reads the saved settings from the config file, if they're there."""
+        if 'DIR' in self.config:
+            self.folder = self.config['DIR']['root_folder']
+            idx = self.fileModel.setRootPath(str(self.folder))
+            self.tree.setRootIndex(idx)
+            self.dirLabel.setText(self.folder)
+
+        if 'REDDIT' in self.config:
+            self.subredditInput.setText(self.config['REDDIT']['subreddit'])
+            self.numInput.setText(self.config['REDDIT']['num'])
+            self.sortingCb.setCurrentText(self.config['REDDIT']['sorting'])
+
+
+
+
+################### Actions: ##################
+    @pyqtSlot(QModelIndex)
+    def on_treeView_clicked(self, index):
+        """triggers when the user clicks on a file item shown in the treeView, and shows that file in the picture viewer."""
+
+        indexItem = self.fileModel.index(index.row(),0,index.parent() )
+        filePath = self.fileModel.filePath(indexItem)
+        pixmap=QPixmap(filePath)
+        self.imgView.setPixmap(pixmap.scaledToHeight(800))
+
+    def showDirDialog(self):
+        """lets the user select the root folder, and saves the choice to the config file."""
+
+        self.folder =QFileDialog.getExistingDirectory(self,'Choose base directory','/home')
+        self.dirLabel.setText(self.folder)
+
+        self.config['DIR']={ 'root_folder' : self.folder }
+        with open('redditScraper.ini', 'w') as configfile:
+            self.config.write(configfile)
+
+        idx= self.fileModel.setRootPath(self.folder)
+        self.tree.setRootIndex(idx)
+
+
+        return self.folder
+
+
+    @pyqtSlot(str)
+    def update_output_text(self, message:str):
+        """updates the output text area, to show progress on downloads."""
+        self.outputText.setText(self.outputText.toPlainText() + message)
+
+    def saveSubreddit(self, subreddit, num, sorting):
+        """helper function to save the current settings to the config file."""
+        self.config['REDDIT'] = {
+            'subreddit': subreddit,
+            'num': str(num),
+            'sorting':sorting
+        }
+        with open('redditScraper.ini', 'w') as configfile:
+            self.config.write(configfile)
+
+    def runDownloadThreaded(self):
+        """downloads the pictures. Runs in a QThread, so that the program does not freeze.
+        Also checks whether the specified subreddit exists. """
+        subreddit = self.subredditInput.text()
+        num = int(self.numInput.text())
+        sorting = self.sortingCb.currentText()
+
+        if self.redditScraper.sub_exists(subreddit):
+            self.saveSubreddit(subreddit,num,sorting)
+            self.get_thread = RedditDownloadThread(self.redditScraper, subreddit, num,
+                                                   sorting, self.folder)
+            self.get_thread.changeText.connect(self.update_output_text)
+            self.get_thread.start()
+        else:
+            msgBox = QMessageBox()
+            msgBox.setText('That subreddit does not exist, please try again')
+            msgBox.setWindowTitle("Invalid subreddit")
+            msgBox.exec_()
+
+        #some check here that we have a valid username+password.
+
+        pass
+
+    def stopDownload(self):
+        """Stops the download thread and prints a message to the output."""
+        if self.get_thread.isRunning():
+            self.get_thread.terminate()
+            self.outputText.setText(self.outputText.toPlainText() + ' Aborted!\n')
+        pass
+
+##### Menu actions: ###############
+    def edit_login_info(self):
+        """let's the user change the reddit login information. Checks so that only valid logins gets saved
+        to the config file. """
+        while True:
+            LoginInfoWindow(self)
+            self.redditScraper = redditScraper.redditScraper(self.username,
+                                                             self.password)
+            if self.redditScraper.valid_login():
+                break
+            else:
+                msgBox = QMessageBox()
+                msgBox.setText('Please provide a valid username and password.')
+                msgBox.setWindowTitle("Invalid information")
+                msgBox.exec_()
+
+        self.config['USER'] = {
+            'username': self.username,
+            'password': self.password
+        }
+
+        with open('redditScraper.ini', 'w') as configfile:
+            self.config.write(configfile)
+
+    def helpClicked(self):
+        msgBox = QMessageBox()
+
+        msgBox.setWindowIcon(QIcon('redditIcon.png'))
+        msgBox.setText('This program downloads images posted to reddit.com, or more specifically, to subreddits' +
+                       '(i.e. sub-forums). To use it, one needs a valid reddit account, which one can sign up for ' +
+                       'on reddit.com. One also needs to know some names of subreddits. \n Some suggestions for subreddits: ' +
+                       'wallpapers , earthporn , nature , and pics . \n \n '
+                       + "This program will download up to the specified number of images. It can handle png, jpg and gif,"
+                        +  "so links to gyf-files, videos or anything else will be ignored. Therefore the number of images actually "
+                         +"downloaded will usually be less than the specified number. So if you want a lot of images, just put"
+                       + " a large limit.\n The images will be placed in a subfolder of the chosen base folder, named after the subreddit. This folder will be created if it does not exist.\n \n "
+                        + "To view the images, click on them in the tree-view, and they will appear on the right" )
+        msgBox.setWindowTitle("Help")
+        msgBox.exec_()
+
+
+class RedditDownloadThread(QThread):
+    """Defines the thread that runs the download."""
+
+    changeText = pyqtSignal(str)
+
+    def __init__(self, redditInstance, sub: str, num: int, sort: str, base_folder:str):
+        QThread.__init__(self)
+        self.reddit=redditInstance
+        self.subreddit = sub
+        self.num = num
+        self.sorting = sort
+        self.base_folder=base_folder
+
+        # self.changeText = pyqtSignal(str)
+
+    def __del__(self):
+        self.wait()
+
+    def run(self):
+        """Runs the actual download, using various helper functions from the redditScraper class."""
+        img_urls = self.reddit.get_image_urls(self.subreddit, self.sorting, self.num)
+        folder = os.path.join(self.base_folder, self.subreddit)
+        date = str(datetime.datetime.now().date())
+
+        self.changeText.emit('Downloading ' + str(
+            len(img_urls)) + ' images from /r/' + self.subreddit + ', sorted by ' + self.sorting + '\n')
+
+        for i, url in enumerate(img_urls):
+            filename = date +' '+self.sorting + ' ' + str(i + 1)
+            self.reddit.download_image(url, filename, folder)
+            self.changeText.emit(str(i+1) + ' ')
+            self.msleep(500)
+
+
+        self.changeText.emit("Finished! \n")
+
+
+
+######### Runs the thing: ############
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = RedditScraperWindow()
+    sys.exit(app.exec_())
---
+++ redditScraper.py
@@ -0,0 +1,115 @@
+#reddit image scraper2, using praw
+
+import praw
+from prawcore import NotFound, OAuthException
+import requests
+import os
+from PIL import Image
+
+
+class redditScraper:
+
+    def __init__(self, username: str, password: str):
+        self.user = username
+        self.password = password
+        self.reddit = praw.Reddit(client_id = 'mtCkwaHIYGEmuA',
+                         client_secret = 'hf4ESLBWdHLgaQ74xGndS53VhG4',
+                         user_agent = 'windows:image_scraper (by /u/hopffiber)',
+                         username = username,
+                         password = password)
+
+        #dictionary for switching between the sorting options:
+        self.sorting_options = {"Top all time": self.top_all_posts,
+                           "Top this month": self.top_month_posts,
+                            "Top past year":self.top_year_posts,
+                            "New": self.new_posts,
+                            "Controversial": self.controversial_posts,
+                           "Hot": self.hot_posts }
+
+
+###### All the various options for sorting ############
+    def controversial_posts(self, sub:str, num:int):
+        subreddit=self.reddit.subreddit(sub)
+        return subreddit.controversial(limit=num)
+    def new_posts(self, sub:str, num:int):
+        subreddit=self.reddit.subreddit(sub)
+        return subreddit.new(limit=num)
+    def top_year_posts(self, sub:str, num:int):
+        subreddit=self.reddit.subreddit(sub)
+        return subreddit.top(limit=num,time_filter='year')
+    def top_all_posts(self, sub:str, num:int):
+        subreddit=self.reddit.subreddit(sub)
+        return subreddit.top(limit=num)
+    def top_month_posts(self,sub:str, num:int):
+        subreddit=self.reddit.subreddit(sub)
+        return subreddit.top(time_filter='month', limit=num)
+    def hot_posts(self,sub:str, num:int):
+        subreddit=self.reddit.subreddit(sub)
+        return subreddit.hot( limit = num)
+##########################################################
+
+
+    def sub_exists(self, sub:str) -> bool:
+        """Tests if a given subreddit exists."""
+        exists = True
+        try:
+            self.reddit.subreddits.search_by_name(sub, exact=True)
+        except NotFound:
+            exists = False
+        return exists
+    def valid_login(self)-> bool:
+        """Tests if the provided login information works for logging into reddit."""
+        try:
+            self.reddit.subreddit('news').hot().next()
+        except OAuthException:
+            return False
+        return True
+
+    def handle_imgur_links(self, imgur_link: str) -> str:
+        """A way of extracting the image from an imgur link, which works most of the time.
+        A more proper way would be to use imgur API, but for now I'm too lazy. """
+        l = imgur_link.split('/')
+        return '/'.join([x if x != 'imgur.com' else 'i.imgur.com' for x in l]) + '.jpg'
+
+    def get_image_urls(self, sub: str, sorting:str, num: int)-> list :
+        """returns a list of up to num links to images."""
+        posts = self.sorting_options[sorting](sub, num)
+
+        urls = [x.url for x in posts]
+
+        image_urls = []
+        for i in range(len(urls)):
+            if urls[i][-4:] == '.jpg' or urls[i][-4:] == '.png' or urls[i][-4:] == '.gif':
+                image_urls.append(urls[i])
+            elif 'imgur.com' in urls[i].split('/'):
+                image_urls.append(self.handle_imgur_links(urls[i]))
+        return image_urls
+
+    def download_image(self, url: str, filename: str, folder: str):
+        """Downloads the image, saves it as 'filename'+('.jpg' or '.png'), in the specified folder"""
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        if url[-4:] == '.jpg' or url[-4:] == '.png' or url[-4:] == '.gif':
+            img_data = requests.get(url).content
+            with open(folder + '\\' + filename + url[-4:], 'wb') as handler:
+                handler.write(img_data)
+                # print('downloaded ' + filename)
+
+
+    def download_images_print(self, sub: str, sorting:str, num: int, base_folder:str):
+        """Downloads the selected images into base_folder/subreddit.
+        Can be used in the console to test that this redditScraper class works as intended. """
+        img_urls = self.get_image_urls(sub, sorting, num)
+
+        folder = os.path.join(base_folder, sub)
+
+        print( 'Downloading '+ str(len(img_urls)) + ' images from ' + sub +', sorted by ' +sorting  )
+        for i, url in enumerate(img_urls):
+            filename = sub + str(i + 1)
+            self.download_image(url, filename, folder)
+            print(str(i), end=" ")
+        print('Finished!' )
+
+
+
+
